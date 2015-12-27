9fe5645701148cb9b9e77bbd568c555759aefd22
Jason Perez <jaeysin@protonmail.com>
2015-12-27 11:19:51 -0800

    Adds support for getting user info from the flickr api and displaying in the DOM. Also move the JSON parse to the get function, handle that in one place. And wrapped the whole buildGallery function in a window.onload, we are modifying the DOM in a bunch of places async, so we should verify the window is loaded  before doing anything.

--- texercise.js
+++ texercise.js
@@ -1,16 +1,62 @@
 "use strict";
 
 (function (){
-    var flickrURL =  'https://api.flickr.com/services/rest/?nojsoncallback=1&format=json';
-    var getPhotosURL =  '&method=flickr.photosets.getPhotos';
-    var getInfoURL =  '&method=flickr.photos.getSizes';
-    var imagesPerPage = 21;
+    var flickrURL         =  'https://api.flickr.com/services/rest/?nojsoncallback=1&format=json';
+    var getPhotosURL      =  '&method=flickr.photosets.getPhotos';
+    var getInfoURL        =  '&method=flickr.photos.getSizes';
+    var peopleGetInfoURL  =  '&method=flickr.people.getInfo';
+    var imagesPerPage   = 21;
     var lightboxImage;
     var lightboxGrid;
     var imageTitle;
     var currentElementInLightbox;
     var browserHeight;
     var browserWidth;
+    var userNameElement;
+    var realNameElement;
+    var userDescriptionElement;
+    var profileUrlElement;
+
+    function buildGallery(params) {
+        window.onload = function(){
+            setupElementRefs();
+            getBrowserHeight();
+
+            if(!params){
+                params = {};
+            }
+
+            if(params.apiKey && params.userId && params.setId){
+                getPhotos(params.apiKey, params.userId, params.setId, addImageToDOM, failure);
+            } else {
+                params = getURLParametersOrUseParams(params);
+                getPhotos(params.apiKey, params.userId, params.setId, addImageToDOM, failure);
+            }
+            //if all parameters are supplied to the buildGallery call, we just use these.
+        }
+        //we should always have a window
+    }
+
+    function setupElementRefs() {
+        var doc = document;
+
+        doc.getElementById('previous').addEventListener('click', goPrevious);
+        doc.getElementById('next').addEventListener('click', goNext);
+        doc.getElementById('close').addEventListener('click', close);
+
+        lightboxGrid = doc.getElementById('image-grid');
+        imageTitle  = doc.getElementById('image-title');
+        lightboxImage = doc.getElementById('lightbox-image');
+
+        userNameElement = doc.getElementById('user-name');
+        realNameElement = doc.getElementById('real-name');
+        userDescriptionElement = doc.getElementById('user-description');
+        profileUrlElement = doc.getElementById('profile-url');
+
+        lightboxImage.addEventListener('click', function(event){
+            event.stopPropagation();
+        });
+    }
 
     function get(url, success, failure, args) {
         var request = new XMLHttpRequest();
@@ -19,18 +65,55 @@
 
         request.onreadystatechange = function(){
             if (request.readyState == 4 && request.status == 200){
-                success(request, args);
+                var response = JSON.parse(request.response);
+                success(response, args);
             }  else if (request.status != 200 && request.status != 304) {
+                //this could have more options and take into account more response codes
+                console.log('err', request);
                 failure(request.status);
             }
         };
     }
     
+    function getUserInfo (apiKey, userId){
+        var url = flickrURL + peopleGetInfoURL + '&api_key=' + apiKey + '&user_id=' + userId;
+
+        function appendUserInfoToDom(response){
+            var person = response.person;
+            var userName = person.username._content;
+            var realName = person.realname._content;
+            var description = person.description._content;
+            var profileUrl = person.profileurl._content;
+
+            if(person){
+
+                if(userName && userNameElement){
+                    userNameElement.innerHTML = userName;
+                }
+
+                if(realName && realNameElement){
+                    realNameElement.innerHTML = realName;
+                }
+
+                if(description && userDescriptionElement){
+                    userDescriptionElement.innerHTML = description;
+                }
+
+                if(profileUrl && profileUrlElement){
+                    profileUrlElement.innerHTML = profileUrl;
+                }
+            }
+
+        }
+        get(url, appendUserInfoToDom, failure);
+    }
+
     function getPhotos(apiKey, userId, setId, success, failure) {
         var url = flickrURL + getPhotosURL + '&api_key=' + apiKey + '&photoset_id=' + setId + '&user_id=' + userId + '&per_page=' + imagesPerPage;
 
-        function parseResponse(request){
-            var parsedResponse = JSON.parse(request.response).photoset;
+        function parseResponse(response){
+            var parsedResponse = response.photoset;
+            getUserInfo(apiKey, userId);  
             getPhotosInfo(apiKey, parsedResponse, success, failure);
         }
 
@@ -40,37 +123,23 @@
     function getPhotosInfo(apiKey, photos, success, failure) {
         var url = flickrURL  + getInfoURL + '&api_key=' + apiKey;
 
-        function parseResponse(request, photoTitle){
-          var parsedResponse  = JSON.parse(request.response);
-          var photoSizes      = parsedResponse.sizes.size;
-          var photoSizeLength = photoSizes.length
+        function parseResponse(response, photoTitle){
+            var parsedResponse  = response;
+            var photoSizes      = parsedResponse.sizes.size;
+            //there could be better control over what images to use
+            //we are using the smallest and nearly the largest
 
-          success({
+            success({
               previewImage: photoSizes[1].source,
               displayImage: photoSizes[9].source,
-              photoTitle: photoTitle
-          });
+              photoTitle:   photoTitle
+            });
         }
 
         photos.photo.forEach(function(photo){
             url = url + '&photo_id=' + photo.id;
             get(url, parseResponse, failure, photo.title);
         });
-    }
-    
-    function setupElementRefs() {
-        window.onload = function(){
-            var doc = document;
-            doc.getElementById('previous').addEventListener('click', goPrevious);
-            doc.getElementById('next').addEventListener('click', goNext);
-            doc.getElementById('close').addEventListener('click', close);
-            lightboxImage = doc.getElementById('lightbox-image');
-            lightboxImage.addEventListener('click', function(event){
-                event.stopPropagation();
-            });
-            lightboxGrid = doc.getElementById('image-grid');
-            imageTitle = doc.getElementById('image-title');
-        };
     }
 
     function goNext() {
@@ -129,9 +198,6 @@
         currentElementInLightbox = event.target.parentElement;
         document.body.setAttribute('class', 'lightbox-active');
         event.stopPropagation();
-        document.body.addEventListener('click', function(e){
-            close();
-        });
         document.body.addEventListener('click', close);
         imageTitle.innerHTML = currentElementInLightbox.dataset.title;
         lightboxImage.setAttribute('style', 'max-height: ' + scaledBrowserHeight + 'px');
@@ -152,23 +218,6 @@
           browserWidth = document.body.clientWidth;
           browserHeight = document.body.clientHeight;
         }
-    }
-
-    function buildGallery(params) {
-        setupElementRefs();
-        getBrowserHeight();
-
-        if(!params){
-            var params = {};
-        }
-
-        if(params.apiKey && params.userId && params.setId){
-            getPhotos(params.apiKey, params.userId, params.setId, addImageToDOM, failure);
-        } else {
-            params = getURLParametersOrUseParams(params);
-            getPhotos(params.apiKey, params.userId, params.setId, addImageToDOM, failure);
-        }
-        //if all parameters are supplied to the buildGallery call, we just use these.
     }
 
     function getURLParametersOrUseParams(params){
